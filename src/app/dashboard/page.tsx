0465ed8c216e88acd3f6d732714a23a177e038b0
SKRTEEEEEE <adanreh.m@gmail.com>
2024-09-09 23:59:01 +0200

    test(app): Recive user of database saved with stripe webhooks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
 import getDbConnection from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Page() {
+    const user = await currentUser()
+
+    //update user clerk
+
+    const email = user?.emailAddresses?.[0].emailAddress ?? ""
     const sql = await getDbConnection()
-    const response = await sql`SELECT version()`
+    const response = await sql`SELECT * FROM users WHERE status = 'cancelled' AND email = ${email}`
     return (
-        <div>Dashboard {response[0].version}</div>
+        <div>Dashboard status: {JSON.stringify(response)}</div>
     );
 }
